34a032ccae632848c2ad3b7f4192bb14465c0c95
mtgband <ikerllona@gmail.com>
2024-11-03 09:38:01 +0100

    cambios

--- src/pages/api/get-info-playlist.json.js
+++ src/pages/api/get-info-playlist.json.js
@@ -1,15 +1,15 @@
-import { allPlaylists, songs as allSongs } from "@/lib/data";
-
 export async function GET({ params, request }) {
-  // get the id from the url search params
-  const { url } = request
-  const urlObject = new URL(url)
-  const id = urlObject.searchParams.get('id')
+  const response = await fetch("https://imanol-llona-music-files.pages.dev/data.json");
+  const data = await response.json();
 
-  const playlist = allPlaylists.find((playlist) => playlist.id === id)
-  const songs = allSongs.filter(song => song.albumId === playlist?.albumId)
+  const { url } = request;
+  const urlObject = new URL(url);
+  const id = urlObject.searchParams.get('id');
+
+  const playlist = data.playlists.find((playlist) => playlist.id === id);
+  const songs = data.songs.filter(song => song.albumId === playlist?.albumId);
 
   return new Response(JSON.stringify({ playlist, songs }), {
     headers: { "content-type": "application/json" },
-  })
+  });
 }
