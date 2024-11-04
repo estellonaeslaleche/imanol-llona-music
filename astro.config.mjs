7e186d80bb98d73bff2e8e02cc27a149836d9562
mtgband <ikerllona@gmail.com>
2024-11-04 13:04:26 +0100

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  })
+  adapter: vercel()
 });
