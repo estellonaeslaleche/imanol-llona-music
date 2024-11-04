94880b02dbfbdfef57dd6b5adc5ea512e6bb1d8a
mtgband <ikerllona@gmail.com>
2024-11-04 16:32:29 +0100

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true, // Esto permite que el servidor escuche en la red local
+    port: 4321, // Puedes cambiar el puerto si prefieres uno diferente
+  },
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
   adapter: vercel()
--- src/components/MusicsTable.tsx
+++ src/components/MusicsTable.tsx
@@ -35,7 +35,7 @@ export const MusicsTable = ({ songs }: Props) => {
         <tr className='text-zinc-400 text-sm'>
           <th className='px-4 py-2'>#</th>
           <th className='px-4 py-2 hover:text-white'>Title</th>
-          <th className='px-4 py-2 hover:text-white'><span className='hidden sm:block'>Album</span></th>
+          <th className='px-4 py-2 hover:text-white hidden sm:table-cell'>Album</th>
           <th className='px-4 py-2 hover:text-white' title='Duration'>
             <TimeIcon />
           </th>
@@ -98,10 +98,10 @@ export const MusicsTable = ({ songs }: Props) => {
                   >
                     {song.title}
                   </h3>
-                  <span>{song.artists.join(', ')}</span>
+                  <span className="">{song.artists.join(', ')}</span>
                 </div>
               </td>
-              <td className='px-4 py-2 '><span className='hidden sm:block'>{song.album}</span></td>
+              <td className='px-4 py-2 hidden sm:table-cell'>{song.album}</td>
               <td className='px-4 py-2 rounded-tr-lg rounded-br-lg'>
                 {song.duration}
               </td>
@@ -112,3 +112,4 @@ export const MusicsTable = ({ songs }: Props) => {
     </table>
   )
 }
+
