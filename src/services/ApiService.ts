1ad564a0869e93c6f3ef9dab74620eb40ce0bd13
mtgband <132903719+estellonaeslaleche@users.noreply.github.com>
2025-01-01 20:26:21 +0100

    Update ApiService.ts

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,7 +1,7 @@
 export function getPlayListInfoById(playListId: number) {
   const baseUrl = import.meta.env.DEV
     ? 'http://localhost:4321'
-    : 'https://imanol-llona-music.vercel.app';
+    : 'https://imanol.llonamusic.com';
 
   return fetch(`${baseUrl}/api/get-info-playlist.json?id=${playListId}`).then(res =>
     res.json()
